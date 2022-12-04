9428231369d4e2906383bfbbd754e9466265ab70
nf313743 <1612339+nf313743@users.noreply.github.com>
2022-12-04 12:16:59 +0000

    Part 2 complete

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,18 @@
+using BuberDinner.Application;
+using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
+
